cb5bcde4161764d6dc01647804a2f193647df89a
Shota Yamazaki <yatohs0601@gmail.com>
2021-02-16 14:13:54 +0900

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrouswerRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+  <BrouswerRouter>
+   <App />,
+  </BrouswerRouter>,
   document.getElementById('root')
 );
 
