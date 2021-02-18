82a7ad77dc5a33468c7f7b5611102e5309f8286c
Shota Yamazaki <yatohs0601@gmail.com>
2021-02-18 16:49:16 +0900

    add Node-Sass

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import '../stylesheets/About.sass';
+
 class About extends React.Component {
     render() {
         return(
             <div className="description">
-                <h1>
-                    Hello Wolrd!
-                </h1>
+                <div className="about-me">
+                    <tr>
+                      <td>Name</td>
+                      <th>Shota Yamazaki</th>
+                    </tr>
+                    <tr>
+                      <td>Birthday</td>
+                      <th>1/6/1998</th>
+                    </tr>
+                    <tr>
+                      <td>Hobby</td>
+                      <th>Soccer, Drone</th>
+                    </tr>
+                </div>
             </div>
         );
     }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ class Navigation extends React.Component {
         return(
             <div className="header">
                 <Navbar bg="light" expand="lg">
-                   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                   <Navbar.Brand href="/">Soccer</Navbar.Brand>
                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                    <Navbar.Collapse id="basic-navbar-nav">
                    <Nav className="mr-auto">
