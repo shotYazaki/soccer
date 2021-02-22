559bc47887e04e064c19fb1423a3bd8e15831a4d
Shota Yamazaki <yatohs0601@gmail.com>
2021-02-22 18:12:40 +0900

    edit About.sass

--- src/components/About.js
+++ src/components/About.js
@@ -6,22 +6,24 @@ class About extends React.Component {
     render() {
         return(
             <div className="description">
+              <div class="about_container">
                 <div className="about-me">
-                    <table>
-                       <tr>
-                         <td>Name</td>
-                         <th>Shota Yamazaki</th>
-                       </tr>
-                       <tr>
-                         <td>Birthday</td>
-                         <th>1/6/1998</th>
-                       </tr>
-                       <tr>
-                         <td>Hobby</td>
-                         <th>Soccer, Drone</th>
-                       </tr>
-                    </table>
+                  <table>
+                    <tr>
+                      <td>Name</td>
+                      <th>Shota Yamazaki</th>
+                    </tr>
+                    <tr>
+                      <td>Birthday</td>
+                      <th>1/6/1998</th>
+                    </tr>
+                    <tr>
+                      <td>Hobby</td>
+                      <th>Soccer, Drone</th>
+                    </tr>
+                  </table>
                 </div>
+              </div>
             </div>
         );
     }
